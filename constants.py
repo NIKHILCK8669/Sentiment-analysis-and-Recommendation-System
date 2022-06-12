713708208166c0a40ef8c9573bd116d75c1452e9
NIKHILCK8669 <105057745+NIKHILCK8669@users.noreply.github.com>
2022-06-12 18:17:53 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,17 @@
+# Constants for Sentiment Classifier
+VECTORIZER_PATH = './pickle/tfidf-vectorizer.pkl'
+SENTIMENT_CLASSIFIER_PATH = './pickle/sentiment-xg-boost-model.pkl'
+PROBABILITY_THRESHOLD = 0.55
+
+# Constants for datapaths
+PRODUCT_REVIEWS_MAPPING_PATH = './dataset/sample30.csv'
+
+# Constants for Recommendation Engine
+RECOMMENDATION_SYSTEM_PATH = './pickle/user_final_rating.pkl'
+
+
+#  ROOT_PATH = "./pickle/"
+ #   MODEL_NAME = "sentiment-xg-boost-model.pkl"
+  #  VECTORIZER = "tfidf-vectorizer.pkl"
+   # RECOMMENDER = "user_final_rating.pkl"
+    #CLEANED_DATA = "cleaned_data.pkl"
